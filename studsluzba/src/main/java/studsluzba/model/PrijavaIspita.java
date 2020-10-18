ae6fe6388caccc95f42e4adc33488fa43f94b592
filip <filip@192.168.1.3>
2020-10-18 14:56:47 +0200

    proba komita

---
+++ studsluzba/src/main/java/studsluzba/model/PrijavaIspita.java
@@ -0,0 +1,53 @@
+package studsluzba.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "prijavaIspit")
+public class PrijavaIspita 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idPrijava;
+	private LocalDate datum;
+	private Ispit ispit;
+	
+	public PrijavaIspita(int idPrijava, LocalDate datum, Ispit ispit) {
+		super();
+		this.idPrijava = idPrijava;
+		this.datum = datum;
+		this.ispit = ispit;
+	}
+
+	public int getIdPrijava() {
+		return idPrijava;
+	}
+
+	public void setIdPrijava(int idPrijava) {
+		this.idPrijava = idPrijava;
+	}
+
+	public LocalDate getDatum() {
+		return datum;
+	}
+
+	public void setDatum(LocalDate datum) {
+		this.datum = datum;
+	}
+
+	public Ispit getIspit() {
+		return ispit;
+	}
+
+	public void setIspit(Ispit ispit) {
+		this.ispit = ispit;
+	}
+	
+	
+}
