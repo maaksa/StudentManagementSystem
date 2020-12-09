d8352c7ca0b13905e4fbcc9b45ece73d88b88207
Nikola Paunovic <nikolapaunovic99@gmail.com>
2020-12-09 13:41:10 +0100

    Male izmene u implementaciji i guiu. Plus sredjivanje koda

--- studsluzba/src/main/java/studsluzba/client/fxmlcontrollers/NewSkolskaController.java
+++ studsluzba/src/main/java/studsluzba/client/fxmlcontrollers/NewSkolskaController.java
@@ -1,19 +1,12 @@
 package studsluzba.client.fxmlcontrollers;
 
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import studsluzba.coders.CoderFactory;
-import studsluzba.coders.CoderType;
-import studsluzba.coders.SimpleCode;
 import studsluzba.model.SkolskaGodina;
-import studsluzba.model.SrednjaSkola;
 import studsluzba.services.SifarniciService;
 import studsluzba.services.SkolskaGodinaService;
 
